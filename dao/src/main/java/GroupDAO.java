8cf11ce93743ea8d571b1205ad4f1e5ded0d8a57
Ershov Alexey <alexey3m@gmail.com>
2018-05-13 23:02:49 +0700

    lesson14

---
+++ dao/src/main/java/GroupDAO.java
@@ -0,0 +1,230 @@
+import exceptions.DaoException;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class GroupDAO {
+    private static final String SELECT_ALL_GROUPS = "SELECT * FROM groups";
+    private static final String SELECT_GROUP_BY_NAME = SELECT_ALL_GROUPS + " WHERE name = ?";
+    private static final String SELECT_GROUP_BY_ID = SELECT_ALL_GROUPS + " WHERE group_id = ?";
+    private static final String SELECT_ACCOUNT_ID_FROM_ACCOUNT_IN_GROUP = "SELECT account_id FROM account_in_group WHERE group_id = ?";
+    private static final String SELECT_ACCOUNT_ID_ADMIN_FROM_GROUPS = "SELECT account_id_admin FROM groups WHERE group_id = ?";
+    private static final String SELECT_GROUP_ID_FROM_GROUPS = "SELECT group_id FROM groups WHERE name = ?";
+    private static final String UPDATE_GROUPS_SET_NAME = "UPDATE groups SET name = ? WHERE group_id = ?";
+    private static final String UPDATE_GROUPS_SET_INFO = "UPDATE groups SET info = ? WHERE group_id = ?";
+    private static final String UPDATE_GROUPS_SET_ACCOUNT_ID_ADMIN = "UPDATE groups SET account_id_admin = ? WHERE group_id = ?";
+    private static final String UPDATE_ACCOUNT_IN_GROUP_SET_ACCOUNT_ID = "UPDATE account_in_group SET account_id = ? WHERE account_id = ? AND group_id = ?";
+    private static final String INSERT_INTO_ACCOUNT_IN_GROUP = "INSERT INTO account_in_group (account_id, group_id) VALUES (?, ?)";
+    private static final String DELETE_FROM_ACCOUNT_IN_GROUP = "DELETE FROM account_in_group WHERE account_id = ? AND group_id = ?";
+    private static final String INSERT_INTO_GROUPS = "INSERT INTO groups (name, info, account_id_admin) VALUES(?, ?, ?)";
+    private static final String DELETE_FROM_GROUPS = "DELETE FROM groups WHERE group_id = ?";
+
+    private Connection connection;
+
+    public GroupDAO() {
+        DBConnect DBConnect = new DBConnect();
+        if (DBConnect.getConnection() == null) {
+            DBConnect.createConnection();
+        }
+        connection = DBConnect.getConnection();
+    }
+
+    public GroupDAO(Connection connection) {
+        this.connection = connection;
+    }
+
+    public boolean create(int accountIdAdmin, String groupName, String info) throws DaoException {
+        try (PreparedStatement preparedStatement = this.connection.prepareStatement(SELECT_GROUP_BY_NAME)) {
+            preparedStatement.setString(1, groupName);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if (!resultSet.next()) {
+                insertRowGroup(accountIdAdmin, groupName, info);
+                return true;
+            } else {
+                throw new DaoException("Group name \"" + groupName + "\" is already used.");
+            }
+        } catch (SQLException e) {
+            throw new DaoException(e);
+        }
+    }
+
+
+    public Group get(int id) {
+        try (PreparedStatement preparedStatementGroup = this.connection.prepareStatement(SELECT_GROUP_BY_ID);
+             PreparedStatement preparedStatementMembers = this.connection.prepareStatement(SELECT_ACCOUNT_ID_FROM_ACCOUNT_IN_GROUP)) {
+            preparedStatementGroup.setInt(1, id);
+            preparedStatementMembers.setInt(1, id);
+            try (ResultSet resultSetGroup = preparedStatementGroup.executeQuery();
+                 ResultSet resultSetMembers = preparedStatementMembers.executeQuery()) {
+                if (resultSetGroup.next()) {
+                    return createGroupFromResult(resultSetGroup, resultSetMembers);
+                }
+            }
+            return null;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public List<Group> getAll() {
+        try (ResultSet resultSetGroups = this.connection.createStatement().executeQuery(SELECT_ALL_GROUPS)) {
+            List<Group> groups = new ArrayList<>();
+            while (resultSetGroups.next()) {
+                try (PreparedStatement preparedStatement = this.connection.prepareStatement(SELECT_ACCOUNT_ID_FROM_ACCOUNT_IN_GROUP)) {
+                    preparedStatement.setInt(1, resultSetGroups.getInt("group_id"));
+                    try (ResultSet resultSetMembers = preparedStatement.executeQuery()) {
+                        groups.add(createGroupFromResult(resultSetGroups, resultSetMembers));
+                    }
+                }
+            }
+            return groups;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public boolean update(Group group) {
+        int id = group.getId();
+        String name = group.getName();
+        if (name != null) {
+            try (PreparedStatement preparedStatement = this.connection.prepareStatement(UPDATE_GROUPS_SET_NAME)) {
+                preparedStatement.setString(1, name);
+                preparedStatement.setInt(2, id);
+                preparedStatement.executeUpdate();
+            } catch (SQLException e) {
+                e.printStackTrace();
+                return false;
+            }
+        }
+        String info = group.getInfo();
+        if (info != null) {
+            try (PreparedStatement preparedStatement = this.connection.prepareStatement(UPDATE_GROUPS_SET_INFO)) {
+                preparedStatement.setString(1, info);
+                preparedStatement.setInt(2, id);
+                preparedStatement.executeUpdate();
+            } catch (SQLException e) {
+                e.printStackTrace();
+                return false;
+            }
+        }
+        int accountIdAdmin = group.getAccountIdAdmin();
+        if (accountIdAdmin != 0) {
+            int currentIdAdmin = 0;
+            try (PreparedStatement preparedStatementSelect = this.connection.prepareStatement(SELECT_ACCOUNT_ID_ADMIN_FROM_GROUPS)) {
+                preparedStatementSelect.setInt(1, id);
+                ResultSet resultSet = preparedStatementSelect.executeQuery();
+                if (resultSet.next()) {
+                    currentIdAdmin = resultSet.getInt("account_id_admin");
+                }
+                try (PreparedStatement preparedStatementUpdate1 = this.connection.prepareStatement(UPDATE_GROUPS_SET_ACCOUNT_ID_ADMIN)) {
+                    preparedStatementUpdate1.setInt(1, accountIdAdmin);
+                    preparedStatementUpdate1.setInt(2, id);
+                    preparedStatementUpdate1.executeUpdate();
+                }
+                try (PreparedStatement preparedStatementUpdate2 = this.connection.prepareStatement(UPDATE_ACCOUNT_IN_GROUP_SET_ACCOUNT_ID)) {
+                    preparedStatementUpdate2.setInt(1, accountIdAdmin);
+                    preparedStatementUpdate2.setInt(2, currentIdAdmin);
+                    preparedStatementUpdate2.setInt(3, id);
+                    preparedStatementUpdate2.executeUpdate();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+                return false;
+            }
+        }
+        try {
+            this.connection.commit();
+            return true;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public boolean addMemberToGroup(int idGroup, int idNewMember) {
+        try (PreparedStatement preparedStatement = this.connection.prepareStatement(INSERT_INTO_ACCOUNT_IN_GROUP)) {
+            preparedStatement.setInt(1, idNewMember);
+            preparedStatement.setInt(2, idGroup);
+            preparedStatement.executeUpdate();
+            this.connection.commit();
+            return true;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public boolean removeMemberFromGroup(int idGroup, int idMemberToDelete) {
+        try (PreparedStatement preparedStatement = this.connection.prepareStatement(DELETE_FROM_ACCOUNT_IN_GROUP)) {
+            preparedStatement.setInt(1, idMemberToDelete);
+            preparedStatement.setInt(2, idGroup);
+            preparedStatement.executeUpdate();
+            this.connection.commit();
+            return true;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public boolean removeGroup(int idGroup) {
+        try (PreparedStatement preparedStatement = this.connection.prepareStatement(DELETE_FROM_GROUPS)) {
+            preparedStatement.setInt(1, idGroup);
+            preparedStatement.executeUpdate();
+            this.connection.commit();
+            return true;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    private boolean insertRowGroup(int accountIdAdmin, String groupName, String info) {
+        try (PreparedStatement preparedStatement = this.connection.prepareStatement(INSERT_INTO_GROUPS)) {
+            preparedStatement.setString(1, groupName);
+            preparedStatement.setString(2, info);
+            preparedStatement.setInt(3, accountIdAdmin);
+            preparedStatement.executeUpdate();
+            int groupIdToInsert = -1;
+            try (PreparedStatement preparedStatementGroupID = this.connection.prepareStatement(SELECT_GROUP_ID_FROM_GROUPS)) {
+                preparedStatementGroupID.setString(1, groupName);
+                ResultSet resultSet = preparedStatementGroupID.executeQuery();
+                if (resultSet.next()) {
+                    groupIdToInsert = resultSet.getInt("group_id");
+                }
+            }
+            try (PreparedStatement preparedStatementInsertAccountsInGroupTable = this.connection
+                    .prepareStatement(INSERT_INTO_ACCOUNT_IN_GROUP)) {
+                preparedStatementInsertAccountsInGroupTable.setInt(1, accountIdAdmin);
+                preparedStatementInsertAccountsInGroupTable.setInt(2, groupIdToInsert);
+                preparedStatementInsertAccountsInGroupTable.executeUpdate();
+            }
+            this.connection.commit();
+            return true;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    private Group createGroupFromResult(ResultSet resultSetGroup, ResultSet resultSetMembers) throws SQLException {
+        Group group = new Group();
+        group.setId(resultSetGroup.getInt("group_id"));
+        group.setName(resultSetGroup.getString("name"));
+        group.setInfo(resultSetGroup.getString("info"));
+        group.setAccountIdAdmin(resultSetGroup.getInt("account_id_admin"));
+        List<Integer> members = new ArrayList<>();
+        while (resultSetMembers.next()) {
+            members.add(resultSetMembers.getInt("account_id"));
+        }
+        members.sort(null);
+        group.setMembersId(members);
+        return group;
+    }
+}
---
+++ dao/src/test/java/AccountDAOTest.java
@@ -0,0 +1,289 @@
+import exceptions.DaoException;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class AccountDAOTest {
+    private Connection connection;
+
+    @Before
+    public void initDB() {
+        try {
+            Properties properties = new Properties();
+            properties.load(this.getClass().getClassLoader().getResourceAsStream("H2connect.properties"));
+            String url = properties.getProperty("database.url");
+            String user = properties.getProperty("database.user");
+            String password = properties.getProperty("database.password");
+            this.connection = DriverManager.getConnection(url, user, password);
+            this.connection.setAutoCommit(false);
+            ScriptRunner runner = new ScriptRunner(connection, true, true);
+            runner.runScript(new BufferedReader(new FileReader("D:/Java/dev/projects/getjavajob/social-network-app/dao/src/test/resources/create-data-model.sql")));
+            runner.runScript(new BufferedReader(new FileReader("D:/Java/dev/projects/getjavajob/social-network-app/dao/src/test/resources/fillDB.sql")));
+        } catch (IOException | SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @After
+    public void terminateTables() {
+        try {
+            this.connection.createStatement().execute("DROP TABLE accounts, account_info, groups, account_in_group");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    @Test
+    public void createTest() throws DaoException {
+        AccountDAO accountDAO = new AccountDAO(connection);
+        boolean result = accountDAO.create("kolya1", "123", "Nikolay", "Malcev",
+                "Nikolaevich", "13.12.1982", "+79999999999", "+79999999999",
+                "Lenina str. 145565", "Lenina 2", "ddd@dd.ru", 12345, "asdf", "-");
+        String expectedResultNewAccount = "Account{id=4, username='kolya1', firstName='Nikolay', lastName='Malcev', " +
+                "middleName='Nikolaevich', birthday='13.12.1982', phonePers='+79999999999', phoneWork='+79999999999', " +
+                "addressPers='Lenina str. 145565', addressWork='Lenina 2', email='ddd@dd.ru', icq=12345, skype='asdf', extra='-'}";
+        assertTrue(result);
+        assertEquals(expectedResultNewAccount, accountDAO.get(4).toString());
+    }
+
+    @Test(expected = DaoException.class)
+    public void createExceptionTest() throws DaoException {
+        AccountDAO accountDAO = new AccountDAO(connection);
+        accountDAO.create("kolya1", "123", "Nikolay", "Malcev",
+                "Nikolaevich", "13.12.1982", "+79999999999", "+79999999999",
+                "Lenina str. 145565", "Lenina 2", "ddd@dd.ru", 12345, "asdf", "-");
+        accountDAO.create("kolya1", "123", "Nikolay", "Malcev",
+                "Nikolaevich", "13.12.1982", "+79999999999", "+79999999999",
+                "Lenina str. 145565", "Lenina 2", "ddd@dd.ru", 12345, "asdf", "-");
+    }
+
+    @Test
+    public void getTest() throws DaoException {
+        Account expected = new Account(4, "kolya1", "123", "Nikolay", "Malcev",
+                "Nikolaevich", "13.12.1982", "+79999999999", "+79999999999",
+                "Lenina str. 145565", "Lenina 2", "ddd@dd.ru", 12345, "asdf", "-");
+        AccountDAO accountDAO = new AccountDAO(connection);
+        accountDAO.create("kolya1", "123", "Nikolay", "Malcev",
+                "Nikolaevich", "13.12.1982", "+79999999999", "+79999999999",
+                "Lenina str. 145565", "Lenina 2", "ddd@dd.ru", 12345, "asdf", "-");
+        assertEquals(expected, accountDAO.get(4));
+    }
+
+    @Test
+    public void getAllTest() throws DaoException {
+        Account account1 = new Account(1, "alex1", "123", "Alexey", "Ershov", "Urievich",
+                "22.07.1988", "+79230000000", "+739121231212", "Krasnoyarsk, Karla Marksa str.",
+                "Krasnoyarsk, Svobodny pr.", "a@a.ru", 1234567890, "aaaaa", "extra information");
+        Account account2 = new Account(2, "ivan1", "1234", "Ivan", "Ivanov", "Alexeyevich", "27.01.1993", "+79230000001", "+739121231212", "Moskow", "Moskow 2", "a2@a.ru", 1234500000, "bbbb", "extra information");
+        Account account3 = new Account(3, "sergey1", "12345", "Sergey", "Nosov", "Ivanovych", "02.09.1987", "+79230000002", "+739121231212", "Sankt Petersburg", "Sankt Petersburg 2", "a3@a.ru", 1234500001, "ccccc", "extra information");
+        List<Account> expected = new ArrayList<>();
+        expected.add(account1);
+        expected.add(account2);
+        expected.add(account3);
+        AccountDAO accountDAO = new AccountDAO(connection);
+        assertEquals(expected, accountDAO.getAll());
+    }
+
+    @Test
+    public void getIdTest() throws DaoException {
+        AccountDAO accountDAO = new AccountDAO(connection);
+        assertEquals(2, accountDAO.getId("ivan1"));
+    }
+
+    @Test
+    public void updatePasswordTest() throws DaoException {
+        AccountDAO accountDAO = new AccountDAO(connection);
+        accountDAO.updatePassword(1, "abc");
+        assertEquals("abc", accountDAO.get(1).getPassword());
+    }
+
+    @Test
+    public void updateAccountInfoFirstNameTest() throws DaoException {
+        Account accountUpdate = new Account();
+        accountUpdate.setId(1);
+        accountUpdate.setFirstName("Ivan");
+
+        AccountDAO accountDAO = new AccountDAO(connection);
+        accountDAO.update(accountUpdate);
+        Account expected = new Account(1, "alex1", "123", "Ivan", "Ershov", "Urievich",
+                "22.07.1988", "+79230000000", "+739121231212", "Krasnoyarsk, Karla Marksa str.",
+                "Krasnoyarsk, Svobodny pr.", "a@a.ru", 1234567890, "aaaaa", "extra information");
+        assertEquals(expected, accountDAO.get(1));
+    }
+
+    @Test
+    public void updateAccountInfoLastNameTest() throws DaoException {
+        Account accountUpdate = new Account();
+        accountUpdate.setId(1);
+        accountUpdate.setLastName("Bondarev");
+
+        AccountDAO accountDAO = new AccountDAO(connection);
+        accountDAO.update(accountUpdate);
+        Account expected = new Account(1, "alex1", "123", "Alexey", "Bondarev", "Urievich",
+                "22.07.1988", "+79230000000", "+739121231212", "Krasnoyarsk, Karla Marksa str.",
+                "Krasnoyarsk, Svobodny pr.", "a@a.ru", 1234567890, "aaaaa", "extra information");
+        assertEquals(expected, accountDAO.get(1));
+    }
+
+    @Test
+    public void updateAccountInfoMiddleNameTest() throws DaoException {
+        Account accountUpdate = new Account();
+        accountUpdate.setId(1);
+        accountUpdate.setMiddleName("Vasilyevich");
+
+        AccountDAO accountDAO = new AccountDAO(connection);
+        accountDAO.update(accountUpdate);
+        Account expected = new Account(1, "alex1", "123", "Alexey", "Ershov", "Vasilyevich",
+                "22.07.1988", "+79230000000", "+739121231212", "Krasnoyarsk, Karla Marksa str.",
+                "Krasnoyarsk, Svobodny pr.", "a@a.ru", 1234567890, "aaaaa", "extra information");
+        assertEquals(expected, accountDAO.get(1));
+    }
+
+    @Test
+    public void updateAccountInfoBirthdayTest() throws DaoException {
+        Account accountUpdate = new Account();
+        accountUpdate.setId(1);
+        accountUpdate.setBirthday("01.01.1990");
+
+        AccountDAO accountDAO = new AccountDAO(connection);
+        accountDAO.update(accountUpdate);
+        Account expected = new Account(1, "alex1", "123", "Alexey", "Ershov", "Urievich",
+                "01.01.1990", "+79230000000", "+739121231212", "Krasnoyarsk, Karla Marksa str.",
+                "Krasnoyarsk, Svobodny pr.", "a@a.ru", 1234567890, "aaaaa", "extra information");
+        assertEquals(expected, accountDAO.get(1));
+    }
+
+    @Test
+    public void updateAccountInfoPhonePersTest() throws DaoException {
+        Account accountUpdate = new Account();
+        accountUpdate.setId(1);
+        accountUpdate.setPhonePers("+79230000001");
+
+        AccountDAO accountDAO = new AccountDAO(connection);
+        accountDAO.update(accountUpdate);
+        Account expected = new Account(1, "alex1", "123", "Alexey", "Ershov", "Urievich",
+                "22.07.1988", "+79230000001", "+739121231212", "Krasnoyarsk, Karla Marksa str.",
+                "Krasnoyarsk, Svobodny pr.", "a@a.ru", 1234567890, "aaaaa", "extra information");
+        assertEquals(expected, accountDAO.get(1));
+    }
+
+    @Test
+    public void updateAccountInfoPhoneWorkTest() throws DaoException {
+        Account accountUpdate = new Account();
+        accountUpdate.setId(1);
+        accountUpdate.setPhoneWork("+79230000002");
+
+        AccountDAO accountDAO = new AccountDAO(connection);
+        accountDAO.update(accountUpdate);
+        Account expected = new Account(1, "alex1", "123", "Alexey", "Ershov", "Urievich",
+                "22.07.1988", "+79230000000", "+79230000002", "Krasnoyarsk, Karla Marksa str.",
+                "Krasnoyarsk, Svobodny pr.", "a@a.ru", 1234567890, "aaaaa", "extra information");
+        assertEquals(expected, accountDAO.get(1));
+    }
+
+    @Test
+    public void updateAccountInfoAddressPersTest() throws DaoException {
+        Account accountUpdate = new Account();
+        accountUpdate.setId(1);
+        accountUpdate.setAddressPers("Tver");
+
+        AccountDAO accountDAO = new AccountDAO(connection);
+        accountDAO.update(accountUpdate);
+        Account expected = new Account(1, "alex1", "123", "Alexey", "Ershov", "Urievich",
+                "22.07.1988", "+79230000000", "+739121231212", "Tver",
+                "Krasnoyarsk, Svobodny pr.", "a@a.ru", 1234567890, "aaaaa", "extra information");
+        assertEquals(expected, accountDAO.get(1));
+    }
+
+    @Test
+    public void updateAccountInfoAddressWorkTest() throws DaoException {
+        Account accountUpdate = new Account();
+        accountUpdate.setId(1);
+        accountUpdate.setAddressWork("Tver");
+
+        AccountDAO accountDAO = new AccountDAO(connection);
+        accountDAO.update(accountUpdate);
+        Account expected = new Account(1, "alex1", "123", "Alexey", "Ershov", "Urievich",
+                "22.07.1988", "+79230000000", "+739121231212", "Krasnoyarsk, Karla Marksa str.",
+                "Tver", "a@a.ru", 1234567890, "aaaaa", "extra information");
+        assertEquals(expected, accountDAO.get(1));
+    }
+
+    @Test
+    public void updateAccountInfoEmailTest() throws DaoException {
+        Account accountUpdate = new Account();
+        accountUpdate.setId(1);
+        accountUpdate.setEmail("b@b.ru");
+
+        AccountDAO accountDAO = new AccountDAO(connection);
+        accountDAO.update(accountUpdate);
+        Account expected = new Account(1, "alex1", "123", "Alexey", "Ershov", "Urievich",
+                "22.07.1988", "+79230000000", "+739121231212", "Krasnoyarsk, Karla Marksa str.",
+                "Krasnoyarsk, Svobodny pr.", "b@b.ru", 1234567890, "aaaaa", "extra information");
+        assertEquals(expected, accountDAO.get(1));
+    }
+
+    @Test
+    public void updateAccountInfoIcqTest() throws DaoException {
+        Account accountUpdate = new Account();
+        accountUpdate.setId(1);
+        accountUpdate.setIcq(12345);
+
+        AccountDAO accountDAO = new AccountDAO(connection);
+        accountDAO.update(accountUpdate);
+        Account expected = new Account(1, "alex1", "123", "Alexey", "Ershov", "Urievich",
+                "22.07.1988", "+79230000000", "+739121231212", "Krasnoyarsk, Karla Marksa str.",
+                "Krasnoyarsk, Svobodny pr.", "a@a.ru", 12345, "aaaaa", "extra information");
+        assertEquals(expected, accountDAO.get(1));
+    }
+
+    @Test
+    public void updateAccountInfoSkypeTest() throws DaoException {
+        Account accountUpdate = new Account();
+        accountUpdate.setId(1);
+        accountUpdate.setSkype("bbbbb");
+
+        AccountDAO accountDAO = new AccountDAO(connection);
+        accountDAO.update(accountUpdate);
+        Account expected = new Account(1, "alex1", "123", "Alexey", "Ershov", "Urievich",
+                "22.07.1988", "+79230000000", "+739121231212", "Krasnoyarsk, Karla Marksa str.",
+                "Krasnoyarsk, Svobodny pr.", "a@a.ru", 1234567890, "bbbbb", "extra information");
+        assertEquals(expected, accountDAO.get(1));
+    }
+
+    @Test
+    public void updateAccountInfoExtraTest() throws DaoException {
+        Account accountUpdate = new Account();
+        accountUpdate.setId(1);
+        accountUpdate.setExtra("-------------");
+
+        AccountDAO accountDAO = new AccountDAO(connection);
+        accountDAO.update(accountUpdate);
+        Account expected = new Account(1, "alex1", "123", "Alexey", "Ershov", "Urievich",
+                "22.07.1988", "+79230000000", "+739121231212", "Krasnoyarsk, Karla Marksa str.",
+                "Krasnoyarsk, Svobodny pr.", "a@a.ru", 1234567890, "aaaaa", "-------------");
+        assertEquals(expected, accountDAO.get(1));
+    }
+
+    @Test
+    public void deleteTest() throws DaoException {
+        AccountDAO accountDAO = new AccountDAO(connection);
+        assertTrue(accountDAO.remove(1));
+        assertEquals(2, accountDAO.getAll().size());
+    }
+}
